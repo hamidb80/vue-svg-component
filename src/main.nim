f95c4bf49cee53db56f57b8d74f4b2ae04f10025
hamidb80 <hr.bolouri@gmail.com>
2021-08-28 11:37:37 +0430

    init

---
+++ src/main.nim
@@ -0,0 +1,73 @@
+import os,
+  xmlparser, xmltree, tables, strtabs,
+  strutils, strformat, sequtils,
+  sugar, std/with
+
+type
+  cssStyles = Table[string, string]
+
+func parseStyles(line: string): cssStyles =
+  collect:
+    for exp in line.split ';':
+      if exp != "":
+        let assgnment = exp.split ':'
+        {assgnment[0].strip: assgnment[1].strip}
+
+func `$`(s: cssStyles, sep = ","): string =
+  let temp = collect:
+    for k, v in s:
+      fmt"{k}: {v}"
+
+  temp.join sep
+
+template multiDel(t: untyped, keys: openArray[string])=
+  for k in keys:
+    del t, k
+
+proc main(fname: string) =
+  let
+    svgEl = loadXml fname # xml tree
+    pathEls = svgEl.findall "path"
+    splittedFname = splitFile fname
+
+  var styles = parseStyles svgel.attr "style"
+  multiDel svgel.attrs, ["style", "class"]
+
+  # remove them
+  multiDel styles, ["width", "height", "fill"]
+
+  for pel in pathels:
+    multiDel pel.attrs, ["style", "id", "class"]
+    pel.attrs[":fill"] = "color"
+
+  let vueFile = newElement("wrapper")
+  with vueFile:
+    add newXmlTree("template", [svgEl])
+    add newXmlTree("script", [newText [
+       "\n",
+      """
+        export default {
+          name: """, "\"i-" & splittedFname.name & "\",\n",
+        """
+          props: {
+            color: String,
+          },
+        }
+      """,
+      "\n"
+    ].join.unindent 4 * 2])
+    add newXmlTree("style", [newText [
+       "\nsvg{\n",
+       `$`(styles, ";\n").indent 2,
+       "\n}\n"
+    ].join], {"scoped": "scoped"}.toXmlAttributes)
+
+
+  writeFile fmt"./output/{splittedfname.name}.vue",
+      vuefile.items.toseq.join("\n\n").replace("&quot;", "\"")
+
+when isMainModule:
+  if paramCount() >= 1:
+    main paramStr 1
+  else:
+    quit "no file input"
---
+++ svg_component_vue.nimble
@@ -0,0 +1,13 @@
+# Package
+
+version       = "0.0.1"
+author        = "hamidb80"
+description   = "a program to convert svg images to vuejs components"
+license       = "MIT"
+srcDir        = "src"
+bin           = @["main"]
+
+
+# Dependencies
+
+requires "nim >= 1.5.1"
