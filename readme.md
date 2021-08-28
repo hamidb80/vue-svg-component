19ef6b8e27985ca118f6190093c85edd9068d84c
hamidb80 <hr.bolouri@gmail.com>
2021-08-28 19:58:20 +0430

    createVueTemplate

---
+++ readme.md
@@ -0,0 +1,8 @@
+# About
+the fonts on the web were not complete and we had to put some extra `SVG`s into the project
+
+if you import a `SVG` as an image, you cant change it's color and you have to import it's xml tree in your `HTML`
+
+so I wrote a script in nim that compiles `SVG` into vue templates
+
+# How To Use
--- src/main.nim
+++ src/main.nim
@@ -1,11 +1,18 @@
 import os,
-  xmlparser, xmltree, 
+  xmlparser, xmltree,
   tables, strtabs,
   strutils, strformat, sequtils,
   sugar, std/with
 
 type
   cssStyles = Table[string, string]
+
+
+template multiDel(t: untyped, keys: openArray[string]) =
+  for k in keys:
+    del t, k
+
+# ---------------------------------------------------------------
 
 func parseStyles(line: string): cssStyles =
   collect:
@@ -21,49 +28,59 @@ func `$`(s: cssStyles, sep = ","): string =
 
   temp.join sep
 
-template multiDel(t: untyped, keys: openArray[string])=
-  for k in keys:
-    del t, k
+# ---------------------------------------------------------------
 
-proc main(fname: string) =
-  let
-    svgEl = loadXml fname # xml tree
-    pathEls = svgEl.findall "path"
-    splittedFname = splitFile fname
-
-  var styles = parseStyles svgel.attr "style"
-  multiDel svgel.attrs, ["style", "class"]
-
-  # remove them
-  multiDel styles, ["width", "height", "fill"]
-
-  for pel in pathels:
-    multiDel pel.attrs, ["id", "style", "fill"]
-
+func createVueTemplate(svgEl: XmlNode, scripts, styles: string): string=
+  
   let vueFile = newElement("wrapper")
   with vueFile:
     add newXmlTree("template", [svgEl])
-    add newXmlTree("script", [newText [
+    add newXmlTree("script", [newText scripts])
+    add newXmlTree("style", [newText styles], {"scoped": "scoped"}.toXmlAttributes)
+
+  # replace escaped " with real "
+  vuefile.items.toseq.join("\n\n").replace("&quot;", "\"")
+
+
+proc main(svgPath, outPath: string) =
+  let
+    svgEl = loadXml svgPath # xml tree
+    splittedFname = splitFile svgPath
+
+  # remove/modify attributes
+  multiDel svgel.attrs, ["class", "style"]
+
+  var styles = parseStyles svgel.attr "style"
+  multiDel styles, ["width", "height", "fill"]
+
+  for pel in svgEl.findall "path":
+    multiDel pel.attrs, ["id", "style", "fill"]
+
+
+  writeFile outpath, createVueTemplate(
+    svgEl,
+    [
       """
 
         export default {
-          name: """, "\"i-" & splittedFname.name & "\",\n",
+          name: """, "\"i-" & splittedfname.name & "\",\n",
         """
         }
       """,
-    ].join.unindent 4 * 2])
-    add newXmlTree("style", [newText [
+    ].join.unindent 4 * 2,
+
+    [
        "\nsvg{\n",
        `$`(styles, ";\n").indent 2,
        "\n}\n"
-    ].join], {"scoped": "scoped"}.toXmlAttributes)
-
-  writeFile fmt"./output/{splittedfname.name}.vue",
-      vuefile.items.toseq.join("\n\n").replace("&quot;", "\"")
+    ].join
+  )
+      
 
 
 when isMainModule:
   if paramCount() >= 1:
-    main paramStr 1
+    main paramStr 1, "output/ex.vue"
+
   else:
     quit "no file input"
